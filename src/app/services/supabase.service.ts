04364d2d128014180335f3ab718f0fcc43c5e975
RMou <ronyjmolina@gmail.com>
2023-12-18 01:10:23 -0600

    variables

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,13 +1,14 @@
 
 import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../../src/environments/environment.prod';
 
 @Injectable({
     providedIn: 'root'
   })
   export class SupabaseService {
-    private supabaseUrl = 'https://oeocvjsxmjrpytostwrc.supabase.co'; 
-    private supabaseApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lb2N2anN4bWpycHl0b3N0d3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1MDI1NDEsImV4cCI6MjAxODA3ODU0MX0.9kiGnBg8IyLxX1NGqhYnwH2rLG4zaS-WffsYSiPbSaA'; 
+    private supabaseUrl = environment.supabaseUrl;
+    private supabaseApiKey = environment.supabaseApiKey; 
     public supabase = createClient(this.supabaseUrl, this.supabaseApiKey);
   
     constructor() { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://oeocvjsxmjrpytostwrc.supabase.co',
+    supabaseApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lb2N2anN4bWpycHl0b3N0d3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1MDI1NDEsImV4cCI6MjAxODA3ODU0MX0.9kiGnBg8IyLxX1NGqhYnwH2rLG4zaS-WffsYSiPbSaA'
+  };
+
